259d6eccfdeba925d086c0a44fe9c62bfa0fcaa2
LAKSHMI KS <LAKSHMI KS@LAPTOP-ME6M6UOK>
2024-04-23 23:50:21 +0530

    This is new program

--- SampleMaven/src/main/java/sampleNew.java
+++ SampleMaven/src/main/java/sampleNew.java
@@ -3,7 +3,7 @@ public class sampleNew {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hai");
+		System.out.println("Hai Hello");
 	}
 
 } 
